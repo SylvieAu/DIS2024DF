98b28650ef956409a37cfd617fd5c064af76d4bb
SylvieAu <123520277+SylvieAu@users.noreply.github.com>
2023-01-27 12:29:12 +1000

    Create mypython01.py

---
+++ Unit2_Create_With_Code/mypython01.py
@@ -0,0 +1,15 @@
+# this is a comment
+"""
+this is a multl
+line comment
+"""
+# Input - asking user to input something and saving variable
+name = # input
+age = input("what is your name?")
+
+# Processing
+# Python cn calculate numbers
+age = 2023 + age
+
+# Output
+print("Hi, " +name, "Your age in 20 years will be", age)
