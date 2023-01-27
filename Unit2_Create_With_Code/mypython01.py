b7f20820c7e7b0b9b4d43a021acb3bb4332b513f
Sylvie Au <28350@stpauls.qld.edu.au>
2023-01-27 13:05:07 +1000

    .

--- Unit2_Create_With_Code/mypython01.py
+++ Unit2_Create_With_Code/mypython01.py
@@ -4,12 +4,13 @@ this is a multl
 line comment
 """
 # Input - asking user to input something and saving variable
-name = # input
-age = input("what is your name?")
-
+name = input("what is your name?")
+age = int(input("what is your age?"))
+print(type(age))
+print(type(age))
 # Processing
 # Python cn calculate numbers
-age = 2023 + age
+age = 20 + age
 
 # Output
 print("Hi, " +name, "Your age in 20 years will be", age)
