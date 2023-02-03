930e3d2f1d93775d5c681adad5ea2523468ef5e6
SylvieAu <123520277+SylvieAu@users.noreply.github.com>
2023-02-03 13:52:38 +1000

    Create list_loop_example.py

---
+++ list_loop_example.py
@@ -0,0 +1,12 @@
+names =["Adam", "Bill", "Chris", "David"]
+myname = "David"
+print(myname[:2])
+for i, char in enumerate(myname):
+    print(i+1,char)
+    
+for name in names:
+    print(name)
+    
+print(type(names))
+
+for index, name in enumerate(names)
